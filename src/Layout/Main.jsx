00d1f3547390aaa02a9b37612818f2df876ceee8
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2023-11-15 16:43:24 +0600

    [ADD:] Flowbite react install and navbar added in the header component

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Navbar from '../Components/Navbar/Navbar';
+import Header from '../Components/Header/Header';
 
 const Main = () => {
     return (
         <div>
-            <Navbar></Navbar>
+            <Header></Header>
         </div>
     );
 };
