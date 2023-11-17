98941b873778eadd60f76dcb555d6446a6b62e93
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2023-11-17 12:28:18 +0600

    [Modify:] Sign up page form modify

--- src/Layout/Other.jsx
+++ src/Layout/Other.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 const Other = () => {
     return (
-        <div className='bg-registration-bg bg-center bg-no-repeat bg-cover h-full w-full flex items-center justify-center scroll-smooth'>
+        <div className='bg-registration-bg bg-center bg-no-repeat bg-cover h-[100vh] w-full flex items-center justify-center scroll-smooth'>
             <Outlet></Outlet>
         </div>
     );
--- src/Pages/Registration/SignUp/SignUp.jsx
+++ src/Pages/Registration/SignUp/SignUp.jsx
@@ -17,49 +17,52 @@ const SignUp = () => {
 
 
     return (
-        <div className='w-[60%] bg-white my-8'>
-            <div className='h- bg-primary p-16'>
-                <h2 className='text-white font-bold text-4xl uppercase text-center'>Are you ready for This challang</h2>
+        <div className='w-[90%] bg-white my-8 flex'>
+            <div className='bg-primary p-16 w-1/2 flex items-center'>
+                <h2 className='text-white font-bold text-4xl uppercase text-center leading-tight tracking-wider'>Are you ready for This challang</h2>
             </div>
-            <form className='flex flex-col justify-center items-center gap-8 p-16' onSubmit={handleSubmit(onSubmit)}>
-                <h1 className='text-4xl text-justify text-secondary'>Sign Up</h1>
-                {/* first label */}
-                <div className='flex items-center gap-8 w-full'>
-                    {/* first name */}
-                    <input className="w-1/2 outline-none border-b border-gray-300 text-gray-900 text-lg block p-2.5" {...register("firstName")} placeholder='First Name' />
-                    {/* last name */}
-                    <input className="w-1/2 outline-none border-b border-gray-300 text-gray-900 text-lg block p-2.5" {...register("lastName")} placeholder='Last Name' />
+            <div className='w-1/2'>
+                <form className='flex flex-col justify-center items-center gap-8 p-8' onSubmit={handleSubmit(onSubmit)}>
+                    <h1 className='text-4xl text-justify text-secondary'>Sign Up</h1>
+                    {/* first label */}
+                    <div className='flex items-center gap-8 w-full'>
+                        {/* full name */}
+                        <input className="w-full outline-none border-b border-gray-300 text-gray-900 text-lg block p-2.5" {...register("fullName")} placeholder='Full Name' />
+                        {/* email */}
+                        <input className="w-full outline-none border-b border-gray-300 text-gray-900 text-lg block p-2.5" {...register("email")} placeholder='Your Email' />
+
+                    </div>
+                        {/* second label */}
+                    <div className='flex items-center gap-8 w-full'>
+                        {/* password */}
+                        <input className="w-full outline-none border-b border-gray-300 text-gray-900 text-lg block p-2.5" {...register("password")} placeholder='Password' />
+                        {/* repeat password */}
+                        <input className="w-full outline-none border-b border-gray-300 text-gray-900 text-lg block p-2.5" {...register("confirmPassword")} placeholder='Repeat Password' />
+                    </div>
+                    <div className='flex items-center gap-8 w-full'>
+                        {/* gender selection */}
+                        <select className="w-full cursor-pointer outline-none border-b border-gray-300 text-gray-900 text-lg block p-2.5"  {...register("gender")}>
+                            <option>Gender select</option>
+                            <option value="female">female</option>
+                            <option value="male">male</option>
+                            <option value="other">other</option>
+                        </select>
+                        {/* photo url */}
+                        <input className="block w-full text-sm text-gray-900 border-b border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" type="file" />
+
+                    </div>
+                    <button type="submit" className="cursor-pointer text-white bg-primary hover:bg-secondary font-medium text-xl w-full sm:w-auto px-10  py-5 text-center ">
+                        Sign Up
+                    </button>
+                </form>
+
+                <div className='text-center'>
+                    ------------OR SIGN UP WITH-----------
                 </div>
-                {/* second label */}
-
-                {/* email */}
-                <input className="w-full outline-none border-b border-gray-300 text-gray-900 text-lg block p-2.5" {...register("email")} placeholder='Your Email' />
-                {/* password */}
-                <input className="w-full outline-none border-b border-gray-300 text-gray-900 text-lg block p-2.5" {...register("password")} placeholder='Password' />
-                {/* repeat password */}
-                <input className="w-full outline-none border-b border-gray-300 text-gray-900 text-lg block p-2.5" {...register("confirmPassword")} placeholder='Repeat Password' />
-                <div className='flex items-center gap-8 w-full'>
-                    {/* gender selection */}
-                    <select className="w-full cursor-pointer outline-none border-b border-gray-300 text-gray-900 text-lg block p-2.5"  {...register("gender")}>
-                        <option>Gender select</option>
-                        <option value="female">female</option>
-                        <option value="male">male</option>
-                        <option value="other">other</option>
-                    </select>
-                    {/* photo url */}
-                    <input className="block w-full text-sm text-gray-900 border-b border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" type="file" />
-
-                </div>
-                <button type="submit" className="cursor-pointer text-white bg-primary hover:bg-secondary font-medium text-xl w-full sm:w-auto px-10  py-5 text-center ">
-                    Sign Up
-                </button>
-            </form>
-
-            <div className='text-center'>
-                ------------OR SIGN UP WITH-----------
+                <SocialMedia></SocialMedia>
+                <h4 className='text-xl text-center mb-8'>Already have an account!! please  <Link className='underline text-primary font-bold' to={'/signIn'}>Sign In <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon></Link></h4>
             </div>
-            <SocialMedia></SocialMedia>
-            <h4 className='text-xl text-center mb-8'>Already have an account!! please  <Link className='underline text-primary font-bold' to={'/signIn'}>Sign In <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon></Link></h4>
+
         </div>
     );
 };
